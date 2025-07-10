5fc18f4757dc4c3e3fa25d23765706a63c81bbfd
Deilyn Zamudio <deilynzamudio@gmail.com>
2025-07-10 16:04:33 -0400

    done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        WelcomeMessage();
+        string userName = UserName();
+        int userNumber = UserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+
+        Result(userName, squaredNumber);
+    }
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program, my friend!");
+    }
+    static string UserName()
+    {
+        Console.Write("I am really glad you came! What is your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int UserNumber()
+    {
+        Console.Write("That is a beautiful name! What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void Result(string name, int square)
+    {
+        Console.WriteLine($"Dear {name}, the square of your number is {square}");
     }
 }
