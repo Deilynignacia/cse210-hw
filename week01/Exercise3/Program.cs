61191dce3b7845e4afac97183a9e413705bbb6b8
Deilyn Zamudio <deilynzamudio@gmail.com>
2025-07-10 15:19:38 -0400

    done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Welcome to the Magic Number game! I think of a number and you try to guess it. What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("IMPOSSIBLE! You must be a mind reader... I guess you won.");
+            }
+
+        }                    
     }
 }
