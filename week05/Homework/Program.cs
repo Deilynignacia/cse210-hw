0c700df85cf598eb8fdaf5fd5bc4d407f8dbf602
Deilyn Zamudio <deilynzamudio@gmail.com>
2025-08-02 03:03:56 -0400

    assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Inti Mamani", "English");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Hetu'u Tuki", "Fractions", "1.2", "2-4");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Kallfü Catrileo", "Chilean Native History", "Spanish persecution of the Mapuches in the Araucanía region.");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
