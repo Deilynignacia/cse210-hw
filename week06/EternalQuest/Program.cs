ca3377c0c8b80b7e53124d1e02d7e31ac4fdd7a7
Deilyn Zamudio <deilynzamudio@gmail.com>
2025-08-12 15:51:24 -0400

    creativity requirements comment

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,10 @@
+// In order to  show creativity and exceed requirements, the program has been gamified with an epic theme. 
+// The goals track system was change into a quests one.  The user begins their journey as an "Apprentice" 
+// to the "Chronicler of Destiny", the narrator of the program, and is intended to level up until becoming 
+// the Chronicler's Chosen (there are ten levels). The score is measured in stars, with an empty star (☆) 
+// representing an unaccomplished quest and a filled star (★) indicating a completed one. This proporcionates
+// the user a gratifying experience, creating the feeling of progration and constant growth.
+
 using System;
 using System.Text;
 
